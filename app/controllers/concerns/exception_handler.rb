d702375002b592d6c36aec8efaefaf7fa47f3d20
jesse lawson <homestarflyer@gmail.com>
2018-01-14 22:20:07 -0700

    write some custome handlers

---
+++ app/controllers/concerns/exception_handler.rb
@@ -0,0 +1,23 @@
+module ExceptionHandler
+  extend ActiveSupport::Concern
+
+  # JSON response with message; Status code 422 - unprocessable entity
+  def four_twenty_two(e)
+    render json: { message: e.message }, status: :unprocessable_entity
+  end
+
+  # JSON response with message; Status code 401 - Unauthorized
+  def four_ninety_eight(e)
+    render json: { message: e.message }, status: :invalid_token
+  end
+
+  # JSON response with message; Status code 401 - Unauthorized
+  def four_zero_one(e)
+    render json: { message: e.message }, status: :invalid_token
+  end
+
+  # JSON response with message; Status code 401 - Unauthorized
+  def unauthorized_request(e)
+    render json: { message: e.message }, status: :unauthorized
+  end
+end
