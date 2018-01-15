3329ac5cb0786f4af882fa8bcdd40ec1ad5a0eea
jesse lawson <homestarflyer@gmail.com>
2018-01-14 21:50:43 -0700

    set up config for cors

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,20 @@ module WecalyApi001
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    
+    # cors config
+    config.middleware.insert_before 0, Rack::Cors do 
+      allow do 
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
+    
+    #autoload lib folder durring production
+    config.eager_load_paths << Rails.root.join('lib')
+    
+    #autoload lib folder durring development
+    config.autoload_paths << Rails.root.join('lib')
+
   end
 end
