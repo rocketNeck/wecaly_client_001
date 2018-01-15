64252449bcf5b741fb1e48d63e4c186a9dc7bd30
jesse lawson <homestarflyer@gmail.com>
2018-01-14 23:05:51 -0700

    added controller methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,48 @@
 class UsersController < ApplicationController
+  skip_before_action :authenticate_request, only: [:login, :register]
 
-    # POST /register
-     def register
-       @user = User.create(user_params)
-      if @user.save
-       response = { message: 'User created successfully'}
-       render json: response, status: :created 
+  def login
+    authenticate params[:email], params[:password]
+  end
+
+  def test
+    render json: {
+      message: "You have passed authentication and authorization test"
+    }
+  end
+
+  # POST /register
+  def register
+      @user = User.create(user_params)
+    if @user.save
+      response = { message: 'User created successfully'}
+      render json: response, status: :created 
+    else
+      render json: @user.errors, status: :bad
+    end 
+  end
+  
+    private
+
+    def authenticate(email, password)
+      command = AuthenticateUser.call(email, password)
+
+      if command.success?
+        render json: {
+          access_token: command.result,
+          message: 'Login Successful'
+        }
       else
-       render json: @user.errors, status: :bad
-      end 
-     end
-   
-     private
-   
-     def user_params
-       params.permit(
-         :name,
-         :email,
-         :password
-       )
-     end
+        render json: { error: command.errors }, status: :unauthorized
+      end
+
+    end
+  
+    def user_params
+      params.permit(
+        :name,
+        :email,
+        :password
+      )
+    end
    end
