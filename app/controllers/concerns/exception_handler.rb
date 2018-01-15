a40bac9223761edfec49b457269412e7237160a8
jesse lawson <homestarflyer@gmail.com>
2018-01-14 22:21:40 -0700

    define custome handlers

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -8,6 +8,26 @@ module ExceptionHandler
   class ExpiredSignature < StandardError; end
   class DecodeError < StandardError; end
 
+  included do
+    # Define custom handlers
+    rescue_from ActiveRecord::RecordInvalid, with: :four_twenty_two
+    rescue_from ExceptionHandler::AuthenticationError, with: :unauthorized_request
+    rescue_from ExceptionHandler::MissingToken, with: :four_twenty_two
+    rescue_from ExceptionHandler::InvalidToken, with: :four_twenty_two
+    rescue_from ExceptionHandler::ExpiredSignature, with: :four_ninety_eight
+    rescue_from ExceptionHandler::DecodeError, with: :four_zero_one
+
+    rescue_from ActiveRecord::RecordNotFound do |e|
+     render json: { message: e.message }, status: :not_found
+    end
+
+    rescue_from ActiveRecord::RecordInvalid do |e|
+      render json: { message: e.message }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
   # JSON response with message; Status code 422 - unprocessable entity
   def four_twenty_two(e)
     render json: { message: e.message }, status: :unprocessable_entity
