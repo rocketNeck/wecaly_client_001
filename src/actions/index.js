58330c57a1f60aa675b9d9eb29a9406946012d67
jesse lawson <homestarflyer@gmail.com>
2018-01-23 23:01:20 -0700

    signinUser action creator and it's todo's

---
+++ src/actions/index.js
@@ -0,0 +1,12 @@
+
+export function signinUser({ email, password }) {
+  // submit email password to server
+
+  //if request is good
+  // - update state to indicate user is authenticated
+  // - save the jwt
+  // - redirect to the route  '/feature'
+
+  //if request if bad
+  // - show an error to the user
+}
